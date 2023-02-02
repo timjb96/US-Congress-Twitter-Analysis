b632f50c0a90206bf5f4663342669498be5db402
timjb96 <112847821+timjb96@users.noreply.github.com>
2023-02-01 19:46:11 -0800

    uploading skeleton of visualizations
    drop down w district map from amcharts
    next steps- connect amcharts to dataset, add key ('has or doesn't have twitter)/(was or was not re-elected) (from dataset)
    create charts to compare predicted re-election rates to actual ones (from ML model)

---
+++ camap.js
@@ -0,0 +1,29 @@
+// Create map instance
+var chart = am4core.create("chartdiv", am4maps.MapChart);
+
+// Set map definition
+chart.geodata = am4geodata_region_usa_congressional_caLow;
+
+// Set projection
+chart.projection = new am4maps.projections.Miller();
+
+// Create map polygon series
+var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+// Make map load polygon (like country names) data from GeoJSON
+polygonSeries.useGeodata = true;
+
+// Configure series
+var polygonTemplate = polygonSeries.mapPolygons.template;
+polygonTemplate.tooltipText = "{name}";
+polygonTemplate.fill = am4core.color("#74B266");
+
+// Create hover state and set alternative fill color
+var hs = polygonTemplate.states.create("hover");
+hs.properties.fill = am4core.color("#367B25");
+
+//code sourced from https://www.amcharts.com/docs/v4/tutorials/using-us-congressional-districts-maps/
+
+//read in data 
+//apply data to the districts within amcharts- how to relate the two?
+//style output
