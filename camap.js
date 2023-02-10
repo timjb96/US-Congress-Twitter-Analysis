60ae694430c76721e098c098b81a6fdabbba4692
timjb96 <112847821+timjb96@users.noreply.github.com>
2023-02-09 16:55:41 -0800

    dropdown functionality (print to console)

--- camap.js
+++ camap.js
@@ -1,6 +1,11 @@
 
-//var selector = select("sessionSelect");
+//drop down selectors
+const selectElement = document.getElementById("sessions");
 
+selectElement.addEventListener("change", function() {
+  const selectedValue = selectElement.value;
+  console.log("Selected value: ", selectedValue);
+});
 
 // Create map instance
 var chart = am4core.create("chartdiv", am4maps.MapChart);
@@ -31,18 +36,23 @@ hs.properties.fill = am4core.color("#367B25");
 const congdata = new Array ("output.js");
 
 //function to change w dropdown
-function sessionchange(selector) {
+function sessionchange(sessionSelect) {
     if ("109th Congress (2005-2007") {
         twitcolor109;
     }
 };
 
+//get the geoJSON data
+//let geojsdata = "https://www.amcharts.com/lib/4/geodata/region/usa/congressional/usaCongressionalLow.js"
+//d3.json(geojsdata).then(function(data) {
+//    console.log(data) 
+//});
 //function to highlight twitter if in congress
-function twitcolor109(JoinedOn) {
-    if (JoinedOn > 2005 && JoinedOn < 2008) {
-        return "#FFB6C1"
+function twitcolor109(JoinedOn, StateDistID, id) {
+    if (JoinedOn > 2005 && JoinedOn < 2008 && StateDistID == id ) {
+     console.log("first if")
     }
-    return "#74B266";
+     console.log("else");
 };
 
 //code sourced from https://www.amcharts.com/docs/v4/tutorials/using-us-congressional-districts-maps/
@@ -50,3 +60,4 @@ function twitcolor109(JoinedOn) {
 //read in data 
 //apply data to the districts within amcharts- how to relate the two?
 //style output
+
